0f8cebdf3a7f89a9814db1652fe4b6a9896f0570
Gh0s7j0k3r <miguel.moises@ymail.com>
2025-05-18 02:19:13 -0300

    Firts commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+
+def exibir_nome():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+    
+def exibir_opcoes():
+    print('Escolha uma opção:')
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app...\n')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+# Mesma lógica, mas utilizando o match-case (Python 3.10+)
+'''def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    
+    match opcao_escolhida:
+        case 1:
+            print('Adicionar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            print('Finalizar app')
+        case _:
+            print('Opção inválida!')'''
+
+def main():
+    exibir_nome()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
